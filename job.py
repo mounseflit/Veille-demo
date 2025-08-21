4d43f6c0d367a9e6fbba7e4a57c8729b9bdbb4e2
Mounsef Litniti <93012372+mounseflit@users.noreply.github.com>
2025-08-21 14:49:18 +0200

    Create job.py

---
+++ job.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import requests
+import datetime
+
+def ping_backend():
+    url = "https://veille-demo.onrender.com/watch" #api
+    
+    try:
+        print(f"{datetime.datetime.now()}: Pinging {url}")
+        response = requests.get(url, timeout=30)
+        print(f"Response: {response.status_code}")
+        
+        if response.status_code == 200:
+            print("Job launched")
+        else:
+            print(f"⚠️ Unexpected status code: {response.status_code}")
+            
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    ping_backend()  # Just ping once, then exit
